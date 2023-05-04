e57cc9b0052f060f2939d1e7129500efb333e290
amadking1234 <amadahmed1234678@gmail.com>
2023-05-05 01:43:01 +0600

    chnges

--- excelport/settings.py
+++ excelport/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$*uz*%swgi5m$i-x_i11t&-zv$eiun&990gq0r!%^h$p@&&y5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
