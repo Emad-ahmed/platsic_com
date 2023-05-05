b984ffc0ea592424789e96da07e89ee6cc4b8a19
amadking1234 <amadahmed1234678@gmail.com>
2023-05-05 14:01:17 +0600

    chnges

--- excelport/settings.py
+++ excelport/settings.py
@@ -134,11 +134,12 @@ import os
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
