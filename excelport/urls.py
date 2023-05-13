6b18e94d95804bc76654c9909bd011a036936558
amadking1234 <amadahmed1234678@gmail.com>
2023-05-14 01:27:14 +0600

    chnges

--- excelport/urls.py
+++ excelport/urls.py
@@ -25,8 +25,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('excelapp.urls'))
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
