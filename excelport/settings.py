57a3f3a0afb376fb68c3dffa2a95400c8c6ff05b
amadking1234 <amadahmed1234678@gmail.com>
2023-05-13 18:21:15 +0600

    chnges

--- excelport/settings.py
+++ excelport/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'excelport.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'plasticdb',
+        'USER': 'emad',
+        'PASSWORD': 'amadking123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # import os
 
