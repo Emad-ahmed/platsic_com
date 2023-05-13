95f7f3a2b13750c17f64ba3fd08573be5a4a0494
amadking1234 <amadahmed1234678@gmail.com>
2023-05-13 16:04:29 +0600

    Chnges

--- excelport/settings.py
+++ excelport/settings.py
@@ -89,7 +89,7 @@ else:
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ExcelPort',
-        'USER': 'shak12',
+        'USER': 'Emad',
         'PASSWORD': 'shak12',
         'HOST': 'localhost',
         'PORT': '',
