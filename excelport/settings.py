62dbf95fb6a15f736e9596ca8ffc93c7e42aa326
amadking1234 <amadahmed1234678@gmail.com>
2023-05-13 21:04:34 +0600

    cgsdjf

--- excelport/settings.py
+++ excelport/settings.py
@@ -77,25 +77,14 @@ WSGI_APPLICATION = 'excelport.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ExcelPort',
-        'USER': 'emad',
-        'PASSWORD': 'amadking123',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
 
 # import os
 
