6231bd600b8fbe190a0a3cccb1db41f4b3ae1b55
amadking1234 <amadahmed1234678@gmail.com>
2023-05-14 00:59:31 +0600

    chnges

--- excelport/settings.py
+++ excelport/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import environ
+# import dj_database_url
+# import environ
 
-env = environ.Env()
+# env = environ.Env()
 
-environ.Env.read_env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'excelport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.sqlite3',
-#                 'NAME': BASE_DIR / 'db.sqlite3',
-#             }
-#     }
+
+DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+    }
 # else:
 #     DATABASES = {
 #     'default': {
@@ -106,9 +106,9 @@ WSGI_APPLICATION = 'excelport.wsgi.application'
 
 
 
-DATABASES = {
-    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
